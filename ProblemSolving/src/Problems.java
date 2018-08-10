c02738d1717404bf623cca724f0a8eb1f251f2f8
IUS <IUS@DESKTOP-G6EMM30>
2018-08-10 22:47:26 +0600

    Write a Java program and compute the sum of the digits of an integer. Input an intger: 25 The sum of the digits is: 7

--- ProblemSolving/src/Problems.java
+++ ProblemSolving/src/Problems.java
@@ -3,16 +3,20 @@ import java.util.Scanner;
 public class Problems {
 	
 	public static void main(String[] args) {
-		int count = 0;
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter any Number Please!");
-		int n = input.nextInt();
+		long n = input.nextLong();
+		System.out.println("The sum of the digits is: "+sumDigits(n));
 		
-		while(n>0)
-		{
-			n = n/10;
-			count++;
-	    }
-		System.out.println(count);
 	}
+	
+	
+    public static int sumDigits(long n) {
+        int sum = 0;
+        while (n != 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
 }
